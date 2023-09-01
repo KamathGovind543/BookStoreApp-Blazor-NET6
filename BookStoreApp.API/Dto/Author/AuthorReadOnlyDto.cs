4e9d479cfd7154be476d1a8d95916e4800371a5e
Govind-Zensark <govind.kamath@zensark.com>
2023-09-01 17:04:25 +0530

    Completed Author API

---
+++ BookStoreApp.API/Dto/Author/AuthorReadOnlyDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreApp.API.Dto.Author
+{
+    public class AuthorReadOnlyDto : BaseDto
+    {
+       
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Bio { get; set; }
+    }
+}
