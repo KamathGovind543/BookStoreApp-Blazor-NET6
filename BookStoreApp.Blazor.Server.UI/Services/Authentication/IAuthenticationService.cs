2e47fc1e5bb96d9c3d4e696b39cf1798b4d3aa8a
Govind-Zensark <govind.kamath@zensark.com>
2023-09-07 14:38:26 +0530

    Completed the Login setup in Blazor.UI

---
+++ BookStoreApp.Blazor.Server.UI/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿
+using BookStoteApp.Blazor.Server.UI.Services.Base;
+
+namespace BookStoreApp.Blazor.Server.UI.Services.Authentication
+{
+    public interface IAuthenticationService
+    {
+        Task<bool> AuthenticateAsync(UserLoginDto loginModel);
+        public Task Logout();
+    }
+}
